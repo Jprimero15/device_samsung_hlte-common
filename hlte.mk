8f54eca8eb9e2082c27a25159c3923494e31cecb
Wang Han <416810799@qq.com>
2020-05-26 20:58:17 +0800

    hlte-common: Switch to TimeKeep
    * SE policies are imported and modified from
       https://github.com/sonyxperiadev/device-sony-sepolicy.
       Modifications are needed because qcom legacy policy
       contains conflicting labels and rules.
    
    Signed-off-by: Joshua Primero <jprimero155@gmail.com>

--- hlte.mk
+++ hlte.mk
@@ -121,6 +121,11 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/thermal-engine-8974.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine-8974.conf
 
+# TimeKeep
+PRODUCT_PACKAGES += \
+    timekeep \
+    TimeKeep
+
 # Vibrator
 PRODUCT_PACKAGES += \
     android.hardware.vibrator@1.0-impl
