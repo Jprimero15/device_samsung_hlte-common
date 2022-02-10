a8975555924050b23f31645239fd55a13a42cbe8
Jprimero15 <jprimero155@gmail.com>
2022-02-10 23:44:12 +0800

    hlte-common: sepolicy: Switch to BOARD_VENDOR_SEPOLICY_DIRS
    * https://github.com/LineageOS/android_system_sepolicy/commit/ec3ac470a98342c13c1fec8d46433c73b08531be
    
    Signed-off-by: Jprimero15 <jprimero155@gmail.com>

--- sepolicy/sepolicy.mk
+++ sepolicy/sepolicy.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2018 The LineageOS Project
+# Copyright (C) 2022 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -15,5 +15,5 @@
 #
 
 # Board specific SELinux policy variable definitions
-BOARD_SEPOLICY_DIRS += \
-    device/samsung/hlte-common/sepolicy/common
+BOARD_VENDOR_SEPOLICY_DIRS += \
+    device/samsung/hlte-common/sepolicy/vendor
